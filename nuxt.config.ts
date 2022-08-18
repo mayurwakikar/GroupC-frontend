c057e409f87de5b8d699139c82a61a59dd95b88c
Dhirajzurule12 <zuruledhiraj@gmail.com>
2022-08-19 00:35:36 +0530

    feat:tailwind css installation done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
-import { defineNuxtConfig } from 'nuxt'
+// import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+// export default defineNuxtConfig({
+//     css: ['~/assets/css/tailwind.css'],
+//     build: {
+//       postcss: {
+//         postcssOptions: require('./postcss.config.js'),
+//       },
+//     }
+// })
+import { defineNuxtConfig } from 'nuxt'
 
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: [
+    "~/assets/css/tailwind.css"
+  ],
 })
