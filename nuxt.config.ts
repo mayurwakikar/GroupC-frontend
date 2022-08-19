912f14cd0f877176361667663183f285c676975a
mayur.wakikar <mayurwakikar17@gmail.com>
2022-08-19 16:20:07 +0530

    feat: added frnotend changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    css: ['~/assets/css/tailwind.css'],
+    build: {
+        postcss: {
+            postcssOptions: require('./postcss.config.js'),
+        },
+    }
 })
