cb4224fc1844b0763fdba1bdce46ff699b5e8873
Dhirajzurule12 <zuruledhiraj@gmail.com>
2022-08-19 11:10:39 +0530

    modified student.vue

--- pages/student.vue
+++ pages/student.vue
@@ -1,1 +1,19 @@
-gi
+<template>
+    <main>
+        <div>
+    <div>
+        </div>
+        <div></div>
+        
+
+    </main>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
