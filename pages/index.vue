94dac7dffcbb6bd6539682ce8bab22270b5af7e0
mayur.wakikar <mayurwakikar17@gmail.com>
2022-08-26 18:00:19 +0530

    feat: added validations

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,68 @@
 
 <template>
 <div>
-      <h1 class="font-bold text-center text-2xl mt-10">Book Management System</h1>
-        <div class="  grid grid-cols-2  ">
-          <div class="border-black  border-2 m-8 p-8">
-            <form method="post" class="bg-gray-400 border-black rounded-lg border-2 px-12">
-            <table>
+      <h1 class="font-extrabold text-center text-5xl mt-8">Book Management System</h1>
+      <div>
+        
+        <div >
+          <div class="border-black  border-4 p-8">
+            <form method="post" class=" border-black rounded-lg border-4 px-12">
                 <h2 class="text-teal-900 text-xl font-bold pt-6">Lets Add a Book To Libaray</h2>
-                <hr />
-                <label class="pt-10 py-10 mt-4" for="id">Book Id</label><br />
-                <input type="text"  id="id" name="id" placeholder="" v-model="book_id"  /><br /><br />
-                <label for="author">Book Name</label><br />
-                <input type="text"  id="name" name="name" placeholder="" v-model="book_name"/><br /><br />
-                <label for="author">Book Author</label><br />
-                <input type="text"  id="author" name="author" placeholder="" v-model="book_author"/><br /><br />
-                <label for="image">Book Image</label><br />
-                <input type="text"  id="image" name="image" placeholder="" v-model="book_image"/><br /><br />
-                <label for="price">Book Price</label><br />
-                <input type="number"  id="price" name="price" placeholder="" v-model="book_price"/><br /><br />
-                <label for="isbn">Book ISbn</label><br />
-                <input type="number"  id="isbn" name="isbn" placeholder="" v-model="book_isbn"/><br /><br />
-                    
+            <table>
+                <label class="pt-10 py-10 mt-4" for="id">Book Id</label>
+                <input class="border-2 border-black" type="text"  id="id" name="id" placeholder="" v-model="formData.book_id" @change="validationId"/><br/>
+                <span v-for="error in v$.book_id.$errors" :key="error.$uid">{{error.$message}}</span>
+                <br/><br/>
+                <label for="name">Book Name</label>
+                <input class="border-2 border-black" type="text"  id="name" name="name" placeholder="" v-model="formData.book_name" @change="validationName"/><br>
+                <span v-for="error in v$.book_name.$errors" :key="error.$uid">{{error.$message}}</span>
+                <br/><br/>
+                <label for="author">Book Author</label>
+                <input class="border-2 border-black" type="text"  id="author" name="author" placeholder="" v-model="formData.book_author" @change="validationAuthor"><br>
+                <span v-for="error in v$.book_author.$errors" :key="error.$uid">{{error.$message}}</span>
+                <br><br>
+                <label for="image">Book Image</label>
+                <input class="border-2 border-black" type="text"  id="image" name="image" placeholder="" v-model="formData.book_image" @change="validationImage"/><br>
+                <span v-for="error in v$.book_image.$errors" :key="error.$uid">{{error.$message}}</span>
+                <br/><br/>
+                <label for="price">Book Price</label>
+                <input class="border-2 border-black" type="number"  id="price" name="price" placeholder="" v-model="formData.book_price" @change="validationPrice"><br>
+                <span v-for="error in v$.book_price.$errors" :key="error.$uid">{{error.$message}}</span>
+                <br><br/>
+                <label for="isbn">Book ISbn</label>
+                <input class="border-2 border-black" type="text"  id="isbn" name="isbn" placeholder="" v-model="formData.book_isbn" @change="validationIsbn"/><br>
+                <span v-for="error in v$.book_isbn.$errors" :key="error.$uid">{{error.$message}}</span>
+                <br/><br/>
+                  <label for="bookidsearch">Your Book</label>
+                    <input class="border-2 border-black" type="text" v-model="formData.book_id" label="bookidsearch" />
+                    <button
+                    class="mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20 px-4"
+                        type="button"
+                        @click="getspecificbook"
+                    >
+                        Get Specific Book
+                    </button>
+
+                <!-- <select name="selectbook" id="selectbook">
+                    <option value="category">Book Category</option>
+                    <option value="novels">Novels</option>
+                    <option value="comics">Comics</option>
+                    <option value="scifi">Science Fiction</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="horror">Horror</option>
+                    <option value="action">Action</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="detective">Detective</option>
+                    <option value="mystery">Mystery</option>
+                    <option value="education">Educational</option>
+                    <option value="history">Historical</option>
+                    <option value="geographical">Geographical</option>
+                    <option value="business">Business</option>
+                    <option value="enterpreneurship">Enterpreneurship</option>
+                    <option value="fitness">Body Fitness</option>
+                    <option value="food">Kitechen Food</option>
+                    <option value="spiritual">Spiritual</option>
+                </select>     -->
                 <div class="mt-10">
                     <button class="py-1 px-5 mr-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3" type="submit" @click="formSubmit">Add Book</button>
                     <button class="py-1 px-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3" type="reset">Reset</button>
@@ -30,6 +72,11 @@
                 </div>
             </table>
         </form>
+        <!-- <div>
+            <p>Errors : </p>
+            <span v-for="error in v$.errors" :key="error.$uid"></span>
+            {{ error.$property }} - {{error.$message}},
+        </div> -->
         </div>
         <div class="border-black  border-2 m-8 p-8">
         <table class="list">
@@ -64,111 +111,101 @@
             </tr>
         </table>
         </div>
+        
     </div>
-    </div>
+      </div>
+</div>
 </template>
-<!-- <script>
-export default {
-    data() {
-        return {
-            isEdit:false,
-            indexEdit:-1,
-            users: [],
-            userAddress:'',
-            user: {
-                ProductName: '',
-                Price: '',
-                Stock: '',
-                Size: '',
-                Image:'',
-            },
-        };
-    },
-    methods: {
-        formSubmit(event) {
-            event.preventDefault();
-            // if(this.user.ProductName==""||this.user.ProductName==null||this.user.Price==""|| this.user.Price==null|| this.user.Stock==""||this.user.Stock==null|| this.user.Color==""||this.user.Color==null)
-            // {alert("please fill all the fields");
-            // this.onReset();
-            // }else{console.log("Hi");}
-            if(this.isEdit==true){
-                this.users[this.indexEdit]=this.user;
-                this.isEdit=false;
-                this.indexEdit=-1;
-            }
-            else{
-                 this.users.push(this.user);
-            }
-           this.user = {
-                ProductName: '',
-                Price: '',
-                Stock: '',
-                Size: '',
-                Image:'',
-            };
-        },
-        onReset(event) {
-            event.preventDefault();
-            this.form.ProductName = "";
-            this.form.Price = "";
-            this.form.Stock = "";
-            this.form.Size = "";
-            this.form.Image = "";
-        },
-        userDelete(index) {
-            this.users.splice(index, 1)
-        },
-        onEdit(index){
-            this.user.ProductName=this.users[index].ProductName;
-            this.user.Price=this.users[index].Price;
-            this.user.Stock=this.users[index].Stock;
-            this.user.Size=this.users[index].Size;
-            this.user.Image=this.users[index].Image;
-            this.isEdit=true;
-            this.indexEdit=index;
-        },
-    }
-}
-</script> -->
 
 
 <script setup>
+import { useVuelidate } from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
+//import { required, minLength, book_id, sameAs, } from "@vuelidate/validators";
+const formData = reactive ({
+    book_id:"",
+    book_name:"",
+    book_author:"",
+    book_image:"",
+    book_price:"",
+    book_isbn:"",
+    // addedone line extra as array 
+    
+});
+const rules = {
+    book_id:{ required },
+    book_name:{ required },
+    book_author:{ required },
+    book_image:{ required },
+    book_price:{ required },
+    book_isbn:{ required },
+};
+
+const v$ = useVuelidate(rules,formData)
+
+// const submitForm = async () => {
+//     const result = await v$.value.$validate();
+//     if(result)
+//     {
+//         alert("successfully done");
+//     }
+//     else
+//     {
+//         alert("form is not submitted");
+//     }
+//     alert("successfully , form is submitted");
+// };
+
 import { ref } from "vue";
 // how define multiple variable/ref in single reactive state
 // Access in html template
-const book_id = ref("");
-const book_name = ref("");
-const book_author = ref("");
-const book_image = ref("");
-const book_price = ref("");
-const book_isbn = ref("");
+// const book_id = ref("");
+// const book_name = ref("");
+// const book_author = ref("");
+// const book_image = ref("");
+// const book_price = ref("");
+// const book_isbn = ref("");
 let state = reactive({
   // allBooks: []
   items: [],
 });
 getalldata();
 
+
 async function  getalldata(){
     state.items = await $fetch("http://localhost:3003/book");
 }
 
-
 // 2nd part 
 async function formSubmit() {
-  console.log("we are in post", book_id.value);
+    const result = await v$.value.$validate();
+
+    event.preventDefault();
+    if(result)
+    {
+        // alert("successfully done");
+    }
+    else
+    {
+        alert("form is not submitted");
+    }
+  console.log("we are in post", formData.book_id);
   const sampleData = {
-    book_id: book_id.value,
-   book_name: book_name.value,
-    book_author:book_author.value,
-    book_image: book_image.value,
-    book_price: book_price.value,
-    book_isbn: book_isbn.value, 
+    book_id: formData.book_id,
+    book_name: formData.book_name,
+    book_author:formData.book_author,
+    book_image: formData.book_image,
+    book_price: formData.book_price,
+    book_isbn: formData.book_isbn, 
   };
+  console.log("sample ",sampleData);
   const response = await $fetch("http://localhost:3003/book", {
     method: "POST",
     body: JSON.stringify(sampleData),
+  }).then((res) =>{
+ console.log( alert("successfully done"));
   });
-//   getalldata();
+  getalldata();
 }
 //Delete Api
 async function deletespecificuser(book_id) {
@@ -178,13 +215,22 @@ async function deletespecificuser(book_id) {
   getalldata();
 }
 
+//get specific user
+
+async function getspecificbook() {
+  console.log(book_id);
+  const response = await $fetch("http://localhost:3003/book/" + book_id);
+  state.items = [response];
+}
+getalldata();
+
 async function editBook(book) {
-  (this.book_id = book.book_id),
-    (this.book_name = book.book_name),
-    (this.book_author= book.book_author),
-    (this.book_image = book.book_image),
-    (this.book_price = book.book_price),
-    (this.book_isbn = book.book_isbn);
+  (formData.book_id = book.book_id),
+    (formData.book_name = book.book_name),
+    (formData.book_author= book.book_author),
+    (formData.book_image = book.book_image),
+    (formData.book_price = book.book_price),
+    (formData.book_isbn = book.book_isbn);
 }
 async function editBookApi(book) {
   // const response = await fetch("http://localhost:3001/user/" + this.user_id, {
@@ -204,5 +250,8 @@ async function editBookApi(book) {
   });
   console.log(response);
   getalldata();
+    
+    
+
 }
 </script>
