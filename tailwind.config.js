452621cffaba44ce1e385875b75a43fa535765b8
mjmaniya <manishjaishwal023@gmail.com>
2022-09-01 16:44:14 +0530

    feat: added features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
 
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+//     "./components/**/*.{js,vue,ts}",
+//     "./layouts/**/*.vue",
+//     "./pages/**/*.vue",
+//     "./plugins/**/*.{js,ts}",
+//     "./nuxt.config.{js,ts}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 
-    './assets/**/*.{vue,js,css}',
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
-
+//     './assets/**/*.{vue,js,css}',
+//     './components/**/*.{vue,js}',
+//     './layouts/**/*.vue',
+//     './pages/**/*.vue',
+//     './plugins/**/*.{js,ts}',
+//     './nuxt.config.{js,ts}',
+//   ],
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
 
 module.exports = {
   content: [
